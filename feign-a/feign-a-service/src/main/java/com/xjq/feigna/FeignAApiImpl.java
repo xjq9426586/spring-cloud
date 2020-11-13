4aa64c7e1f799b00fb8a43e1520d50b6e8a21d58
xjq <931408673@qq.com>
2020-11-13 14:05:56 +0800

    修改

--- feign-a/feign-a-service/src/main/java/com/xjq/feigna/FeignAApiImpl.java
+++ feign-a/feign-a-service/src/main/java/com/xjq/feigna/FeignAApiImpl.java
@@ -1,6 +1,9 @@
 package com.xjq.feigna;
 
+import com.xjq.trace.TraceThreadPoolExecutor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
@@ -8,6 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Map;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @Author: xujunqian
@@ -18,10 +23,23 @@ import java.util.Map;
 @RestController
 @Service("FeignAApi")
 public class FeignAApiImpl implements FeignAApi{
+    public static final TraceThreadPoolExecutor threadPool = new TraceThreadPoolExecutor(5, 5, 60, TimeUnit.SECONDS, new LinkedBlockingQueue<>(100));
 
     @Override
     public String file(@RequestPart("file") MultipartFile file, @RequestParam Map<String, Object> params) {
         log.info("文件服务接口");
+        this.sendMsgBySpring();
+        this.sendMsgByThreadPool();
         return "123";
     }
+
+    @Async
+    public void sendMsgBySpring() {
+        log.info("send msg by spring success");
+    }
+
+    public void sendMsgByThreadPool() {
+        threadPool.execute(()->log.info("send msg by thread pool success"));
+    }
+
 }
